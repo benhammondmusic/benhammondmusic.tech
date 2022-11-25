c2705d728a3fbd0a49bfc8ef9f51e0928288633c
Ben Hammond <benjamin.hammond@gmail.com>
2022-11-25 09:46:02 -0700

    fix: colors a11y

--- src/astro-boilerplate-components/components/Logo.tsx
+++ src/astro-boilerplate-components/components/Logo.tsx
@@ -6,7 +6,7 @@ type ILogoProps = {
 };
 
 const Logo = (props: ILogoProps) => (
-  <div className="flex items-center bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-xl font-bold text-transparent">
+  <div className="flex items-center bg-gradient-to-br from-benhammondblue-50 to-cyan-300 bg-clip-text text-xl font-bold text-transparent">
     {props.icon}
 
     {props.name}
