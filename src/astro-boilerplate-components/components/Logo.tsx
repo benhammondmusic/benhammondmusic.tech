66721f55cfaa26bd25865776119eb2e0dab89776
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-10 14:15:24 -0700

    fix: color of logo header

--- src/astro-boilerplate-components/components/Logo.tsx
+++ src/astro-boilerplate-components/components/Logo.tsx
@@ -1,16 +1,16 @@
-import type { ReactNode } from 'react';
+import type { ReactNode } from 'react'
 
 type ILogoProps = {
-  icon?: ReactNode;
-  name: string;
-};
+  icon?: ReactNode
+  name: string
+}
 
 const Logo = (props: ILogoProps) => (
-  <div className="flex items-center bg-gradient-to-br from-benhammondblue-50 to-cyan-300 bg-clip-text text-xl font-bold text-transparent">
+  <div className='flex bg-gradient-to-br to-benhammondyellow-50 from-white bg-clip-text  text-transparent'>
     {props.icon}
 
     {props.name}
   </div>
-);
+)
 
-export { Logo };
+export { Logo }
