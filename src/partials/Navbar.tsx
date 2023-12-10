e4e2d21f8c3177f1c8046d0e0ac16eab471416c9
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-10 14:15:24 -0700

    fix: color of logo header

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -4,27 +4,27 @@ import {
   NavMenu,
   NavMenuItem,
   Section,
-} from '../astro-boilerplate-components';
+} from '../astro-boilerplate-components'
 
 const Navbar = () => (
   <Section>
     <NavbarTwoColumns>
-      <a href="/">
-        <Logo name="benhammond.tech" />
+      <a href='/'>
+        <Logo name='benhammond.tech' />
       </a>
 
       <NavMenu>
-        <NavMenuItem href="https://github.com/benhammondmusic">
+        <NavMenuItem href='https://github.com/benhammondmusic'>
           GitHub
         </NavMenuItem>
-        <NavMenuItem href="https://www.linkedin.com/in/benhammondmusic/">
+        <NavMenuItem href='https://www.linkedin.com/in/benhammondmusic/'>
           LinkedIn
         </NavMenuItem>
-        <NavMenuItem href="https://blog.benhammond.tech">Blog</NavMenuItem>
-        <NavMenuItem href="https://benhammondmusic.com">Music</NavMenuItem>
+        <NavMenuItem href='https://blog.benhammond.tech'>Blog</NavMenuItem>
+        <NavMenuItem href='https://benhammondmusic.com'>Music</NavMenuItem>
       </NavMenu>
     </NavbarTwoColumns>
   </Section>
-);
+)
 
-export { Navbar };
+export { Navbar }
