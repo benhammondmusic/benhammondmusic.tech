1e06f1f81fcf41767792ed6b9308836f1d39bd48
Ben Hammond <benjamin.hammond@gmail.com>
2024-12-25 22:28:02 -0700

    footer

--- src/astro-boilerplate-components/components/FooterCopyright.tsx
+++ src/astro-boilerplate-components/components/FooterCopyright.tsx
@@ -1,22 +1,22 @@
 type IFooterCopyrightProps = {
-  site_name: string
+	site_name: string
 }
 
 const FooterCopyright = (props: IFooterCopyrightProps) => (
-  <div className='border-t border-benhammondblue-50 pt-5 text-center'>
-    <div className='text-sm text-gray-200'>
-      © Copyright {new Date().getFullYear()} by {props.site_name} &{' '}
-      <a
-        className='hover:text-benhammondyellow hover:underline'
-        href='https://github.com/ixartz/Astro-boilerplate'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        CreativeDesignsGuru
-      </a>
-      .
-    </div>
-  </div>
+	<div className='border-t border-benhammondblue-50 pt-5 text-center'>
+		<div className='text-sm text-gray-200'>
+			© Copyright {new Date().getFullYear()} by {props.site_name}. Template forked from{' '}
+			<a
+				className='hover:text-benhammondyellow hover:underline'
+				href='https://github.com/ixartz/Astro-boilerplate'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				CreativeDesignsGuru
+			</a>
+			.
+		</div>
+	</div>
 )
 
 export { FooterCopyright }
