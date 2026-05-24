422a1438221704976aa72d3c15f13bdb3dbb8be6
Ben Hammond <benjamin.hammond@gmail.com>
2026-05-24 07:34:30 -0600

    show title fallback in BlogCard when no cover image

--- src/astro-boilerplate-components/components/BlogCard.tsx
+++ src/astro-boilerplate-components/components/BlogCard.tsx
@@ -1,7 +1,7 @@
 type IBlogCardProps = {
   postSlug: string
   postTitle: string
-  postImgUrl: string
+  postImgUrl?: string
 }
 
 const BlogCard = (props: IBlogCardProps) => (
@@ -9,15 +9,23 @@ const BlogCard = (props: IBlogCardProps) => (
     className='hover:translate-y-1 focus:translate-y-1'
     href={`/blog/${props.postSlug}`}
   >
-    <div className='overflow-hidden rounded-md bg-slate-800'>
-      <div className='aspect-w-2 aspect-h-1'>
-        <img
-          className='h-full w-full object-cover object-center'
-          src={props.postImgUrl}
-          alt={props.postTitle}
-          loading='lazy'
-        />
-      </div>
+    <div className='overflow-hidden rounded-md bg-slate-800 h-full'>
+      {props.postImgUrl ? (
+        <div className='aspect-w-2 aspect-h-1'>
+          <img
+            className='h-full w-full object-cover object-center'
+            src={props.postImgUrl}
+            alt={props.postTitle}
+            loading='lazy'
+          />
+        </div>
+      ) : (
+        <div className='aspect-w-2 aspect-h-1 flex items-center justify-center bg-gradient-to-br from-benhammondblue-900 to-benhammondblue p-4'>
+          <span className='text-sm font-semibold text-center text-gray-200 line-clamp-3'>
+            {props.postTitle}
+          </span>
+        </div>
+      )}
     </div>
   </a>
 )
